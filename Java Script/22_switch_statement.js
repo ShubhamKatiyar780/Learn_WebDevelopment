352aed9ebfd67ca07e57ebc75cc6c7b00be38e15
SHUBHAM KATIYAR <168412256+ShubhamKatiyar780@users.noreply.github.com>
2025-08-22 15:15:04 +0530

    switch statement

---
+++ Java Script/22_switch_statement.js
@@ -0,0 +1,59 @@
+switch(new Date().getDay()){
+    case 0:
+        day = 'Sunday';
+        break;
+    case 1:
+        day = 'Monday';
+        break;
+    case 2:
+        day = 'Tuesday';
+        break;
+    case 3:
+        day = 'Wednesday';
+        break;
+    case 4:
+        day = 'Thursday';
+        break;
+    case 5:
+        day = 'Friday';
+        break;
+    case 6:
+        day = 'Saturday';
+        break;
+    default:
+        day = "Looking forward to the Weekend";
+// If default is not the last case in the switch block, remember to end the default case with a break.
+}
+console.log(day);
+
+// Sometimes you will want different switch cases to use the same code
+switch (new Date().getDay()) {
+  case 4:
+  case 5:
+    text = "Soon it is Weekend";
+    break;
+  case 0:
+  case 6:
+    text = "It is Weekend";
+    break;
+  default:
+      text = "Looking forward to the Weekend";
+}
+console.log(text);
+
+
+// Switch cases use strict comparison (===).
+let x = "0";
+switch (x) {
+  case 0:
+    text = "Off";
+    break;
+  case 1:
+    text = "On";
+    break;
+  default:
+    text = "No value found";
+}
+console.log(text);
+
+
